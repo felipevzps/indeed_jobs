64ff403490d9aa7d13e1f7a89f33d85dcd2f71c0
felipevzps <felipe.vzps@gmail.com>
2020-12-01 05:45:39 -0300

    Added: 1) data and plot directories, 2) fixed: plotting company and location, 3) fixed: getting indeed job links

--- data_visualization.py
+++ data_visualization.py
@@ -1,21 +1,28 @@
+import os
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import nltk
-from nltk import word_tokenize
-from nltk.corpus import stopwords
 
-df1 = pd.read_csv("./bioinfo.csv", encoding="utf-8")
+relative_data_path = "./data/bioinfo12012020_05:15.csv"
+fname = os.path.basename(relative_data_path)
+
+df1 = pd.read_csv(relative_data_path, encoding="utf-8")
 
 df = df1.drop_duplicates()
 print(df.shape)
 
 #Task 1: Find companies with highest number of job listings
-counts = df.groupby("Empresa").count()["Titulo"].sort_values(ascending=False)#[:20]
-#plt.show(counts.plot(kind="bar", figsize=(30,15)))
+fig1 = plt.gcf()
+counts = df.groupby("Company").count()["Title"].sort_values(ascending=False)#[:20]
+plt.show(counts.plot(kind="bar", figsize=(30,15)))
+fig1.tight_layout()
+fig1.savefig("./plot/company_"+fname+".pdf")
 
 #Task 2: Find Locations with highest number of job listings
-loc_counts = df.groupby("Local").count()["Titulo"].sort_values(ascending=False)[:30]
-#plt.show(loc_counts.plot(kind="bar", figsize=(30,15)))
+fig2 = plt.gcf()
+loc_counts = df.groupby("Location").count()["Title"].sort_values(ascending=False)[:30]
+plt.show(loc_counts.plot(kind="bar", figsize=(30,15)))
+fig2.tight_layout()
+fig2.savefig("./plot/location_"+fname+".pdf")
+
 
 
--- get_jobs.py
+++ get_jobs.py
@@ -1,22 +1,28 @@
 from selenium import webdriver
 import pandas as pd 
 from bs4 import BeautifulSoup
+import time
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome("./chromedriver")
 
+# Getting today and time to save new files names 
+today = time.strftime("%m%d%Y_%H:%M")
+
 # Having problems to get job_desc
-df = pd.DataFrame(columns=["Title","Location","Company","Salary","Rating", "Summary"]) #,"Description"])
+df = pd.DataFrame(columns=["Title","Location","Company","Salary","Rating", "Summary","Description"])
 
 for i in range(0,100,10):
 	# Check the link, it must start with 0 and go from 10 to 10
 	# Every multiple of 10 is a page, e.g 0 = page 1, 10 = page 2 ...
-	driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&vjk=3be104f34b880df7'.format(i))
+	#driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&vjk=3be104f34b880df7'.format(i))
+	driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&vjk=d2b226b7af08c0da'.format(i))
+
 	jobs = []
 	driver.implicitly_wait(4)
 	# The each result appears in the class "<div class="jobsearch-SerpJobCard unifiedRow row result clickcard..."
 	# Im getting the attributes from the class wich have "result" in it
-	for job in driver.find_elements_by_class_name('result'):
+	for job in driver.find_elements_by_class_name('result'):# and driver.find_elements_by_id('vjs-container'):
 		soup = BeautifulSoup(job.get_attribute('innerHTML'),'html.parser')
 		try:
 			title = soup.find(class_="title").text.replace("\n","").strip()
@@ -52,19 +58,29 @@ for i in range(0,100,10):
 			close_button.click()
 			sum_div.click()	
 		
+		####
 		# Im having problems to get job_desc :(
+		
+		try:
+			job_desc = soup.find(class_="jobsearch-JobComponent-embeddedBody").text.replace("\n","").strip()
+		except:
+			job_desc = 'None'	
+
 		#job_desc = driver.find_element_by_id("jobDescriptionText").text
 		#job_desc = driver.find_elements_by_id("description").text.replace("\n","").strip()
+		
+		####
+
 		try:
 			summary = soup.find(class_="summary").text.replace("\n","").strip()
 		except:
 			summary = 'None'
 	
 		df = df.append({'Title':title,'Location':location,"Company":company,"Salary":salary,
-						"Rating":rating, "Summary":summary
+						"Rating":rating, "Summary":summary, "Description": job_desc
                         },ignore_index=True)
 
 		print("Got these many results:",df.shape)
 
 
-df.to_csv("bioinfo.csv",index=False)	
+df.to_csv("./data/bioinfo"+today+".csv",index=False)	
