e1868f58a81d66c682fa3562f331dc9656643f39
felipevzps <felipe.vzps@gmail.com>
2023-12-13 16:02:50 -0300

    update selenium sintaxe - working for version 4.10 +

--- get_jobs.py
+++ get_jobs.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
 import pandas as pd 
 from bs4 import BeautifulSoup
 import time
 
+service = Service(executable_path='./chromedriver')
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Chrome(service=service, options=options)
+
+#driver = webdriver.Chrome("./chromedriver", options=options)
 
 # Getting today and time to save new files names 
 today = time.strftime("%m%d%Y_%H:%M")
-output = "Bioinformatics_Remote_112023"
+output = "Bioinformatics_Remote_December"
 
 # Columns
 df = pd.DataFrame(columns=["Title", "Location", "Company", "Salary", "Summary"])
@@ -24,14 +31,15 @@ for i in range(0,90,10):
  	#driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&vjk=d2b226b7af08c0da'.format(i))
     
     # Bioinformatics, remote november
-	driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&pp=gQAPAAAAAAAAAAAAAAACGIfb6gASAQEBCC00UWI2Nj68A0UMyPfCAAA&vjk=adbce905d0ea898e'.format(i))
-     
+	#driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&pp=gQAPAAAAAAAAAAAAAAACGIfb6gASAQEBCC00UWI2Nj68A0UMyPfCAAA&vjk=adbce905d0ea898e'.format(i))
+    # Bioinformatics, remote december
+	driver.get('https://www.indeed.com/jobs?q=bioinformatics&l=Remote&sc=0kf%3Aattr%28DSQF7%29%3B&start={}&vjk=f4a3f4c434b0c649'.format(i))
 	jobs = []
     
 	driver.implicitly_wait(100)
 	
  	# Each result appears in the class "<div class="job_seen_beacon"
-	for job in driver.find_elements_by_class_name('job_seen_beacon'):
+	for job in driver.find_elements(By.CLASS_NAME,'job_seen_beacon'):
 		soup = BeautifulSoup(job.get_attribute('innerHTML'),'html.parser')
 		
 		#title done
