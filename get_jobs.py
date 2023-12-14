d3bdeb1e71d9868ea8be1a34f26f16a25052d4a7
felipevzps <felipe.vzps@gmail.com>
2023-12-14 13:47:38 -0300

    added argparse, removed FutureWarning from pandas

--- get_jobs.py
+++ get_jobs.py
@@ -1,37 +1,43 @@
+#!/usr/bin/env python
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import pandas as pd 
 from bs4 import BeautifulSoup
-import time
+import warnings
+import argparse
+
+parser = argparse.ArgumentParser(description="Web scraping job listings on Indeed.")
+parser.add_argument("--url", dest = 'url', type=str, metavar="URL", required=True, help="URL pattern for web scraping with a {} placeholder")
+parser.add_argument("--end", dest='end', type=int, metavar="40", required=True, help="Last page for iteration")
+parser.add_argument("--out", dest='output', type=str, metavar="joblisting", required=True, help="Output name <saved on data/>")
+
+args = parser.parse_args()
+url = args.url
+end = args.end
+output = args.output
+
+# Ignore FutureWarnings from pandas update (2.0)
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 service = Service(executable_path='./chromedriver')
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
-#driver = webdriver.Chrome("./chromedriver", options=options)
-
-# Getting today and time to save new files names 
-today = time.strftime("%m%d%Y_%H:%M")
-output = "Bioinformatics_Remote_December"
-
 # Columns
 df = pd.DataFrame(columns=["Title", "Location", "Company", "Salary", "Summary"])
 
-for i in range(0,90,10):
+for i in range(0,end,10):
     
 	'''
  	Check the link before running, $i must start with 0 and increases by 10
 	Every multiple of 10 is a page, (e.g 0 = page 1, 10 = page 2 ...)
  	'''
 	
- 	#Bioinformatics - Remote
- 	#driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&vjk=d2b226b7af08c0da'.format(i))
-    
-    # Bioinformatics, remote november
-	#driver.get('https://www.indeed.com/jobs?q=Bioinformatics&l=remote&start={}&pp=gQAPAAAAAAAAAAAAAAACGIfb6gASAQEBCC00UWI2Nj68A0UMyPfCAAA&vjk=adbce905d0ea898e'.format(i))
-    # Bioinformatics, remote december
-	driver.get('https://www.indeed.com/jobs?q=bioinformatics&l=Remote&sc=0kf%3Aattr%28DSQF7%29%3B&start={}&vjk=f4a3f4c434b0c649'.format(i))
+ 	# Bioinformatics, Remote - December
+	#driver.get('https://www.indeed.com/jobs?q=bioinformatics&l=Remote&sc=0kf%3Aattr%28DSQF7%29%3B&start={}&vjk=f4a3f4c434b0c649'.format(i))
+	driver.get(url.format(i))
 	jobs = []
     
 	driver.implicitly_wait(100)
@@ -71,16 +77,8 @@ for i in range(0,90,10):
 		except:
 			summary = 'None'
 
-  		#### cookie button ####
-  
+  		# *** cookie button ***
 		#sum_div = job.find_elements_by_class_name("title")[0]
-
-		#try:
-			#cookie_button.click()
-		#except:
-			#cookie_button = driver.find_element_by_xpath("//button[@id='onetrust-accept-btn-handler']")
-			#cookie_button.click()
-			#sum_div.click()
 		
 		#try:
 			#close_button.click()
@@ -89,11 +87,9 @@ for i in range(0,90,10):
 			#close_button = driver.find_elements_by_class_name('popover-x-button-close')[0]
 			#close_button.click()
 			#sum_div.click()	
-   
-		#### cookie button ####
 		
 		df = df.append({"Title":title, "Location":location, "Company":company, "Salary":salary, "Summary": summary}, ignore_index=True)
 
-		print("Got these many results:", df.shape)
+		print("Got these results:", df.shape[0], "-", title)
 
 df.to_csv("./data/" + output + ".csv", index=False)
